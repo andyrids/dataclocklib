b0dfcd72adb3e5137659ae1892d742acf88ac224
Andrew Ridyard <ridyard12@gmail.com>
2025-01-21 23:37:20 +0000

    chore: refactor assign & aggregate temporal column functions.

--- src/dataclocklib/utility.py
+++ src/dataclocklib/utility.py
@@ -31,7 +31,7 @@ Constants:
 
 import math
 from collections import defaultdict
-from typing import Optional, Tuple, get_args
+from typing import Iterable, Optional, Tuple, get_args, Sequence
 
 import numpy as np
 from dataclocklib.typing import Aggregation
@@ -43,9 +43,10 @@ from matplotlib.colors import Normalize
 from matplotlib.figure import Figure
 from matplotlib.lines import Line2D
 from matplotlib.text import Text
-from numpy.typing import DTypeLike
+from numpy.typing import DTypeLike, NDArray
 from pandas import DataFrame, MultiIndex
 
+from dataclocklib.exceptions import ModeError
 from dataclocklib.typing import CmapNames, FontStyle, Mode
 
 VALID_STYLES: Tuple[FontStyle, ...] = get_args(FontStyle)
@@ -92,6 +93,65 @@ def add_colorbar(
     return colorbar
 
 
+def add_wedge_labels(
+        ax: Axes,
+        font_scale_factor: float,
+        ring_scale_factor: float,
+        ring_text_spacing: float,
+        max_radius: int,
+        theta: NDArray,
+        width: float,
+        wedge_labels: Sequence[str]
+) -> None:
+    """Add scaled and rotated labels around each data clock wedge.
+
+    Labels are placed using Axes.text to facilitate custom rotation
+    of the text, which is based on the angle of the wedge being
+    annotated. The text is scaled based on the size of the chart
+    Figure and padded away from the polar axis based on the number
+    of rings in the chart.
+
+    Args:
+        ax (Axes): Chart Axis.
+        font_scale_factor (float): Scale factor based on current figure size.
+        ring_scale_factor (float): Scale factor based on number of rings.
+        ring_text_spacing (float): Text label distance from polar axis.
+        max_radius (int): Maximum radius (unique rings + 1).
+        theta (NDArray): Angles (radians) for each data clock wedge.
+        width (float): Width of each wedge (2 * Pi / number of wedges).
+        wedge_labels (Sequence[str]): Label text for each wedge.
+
+        Returns:
+            None
+    """
+    if ring_scale_factor > 3:
+        ring_text_spacing = ring_text_spacing * (ring_scale_factor**0.61)
+    else:
+        ring_text_spacing = ring_text_spacing * ring_scale_factor
+
+    # place labels in the centre of each wedge
+    for idx, angle in enumerate(theta + width / 2):
+        # convert to degrees for text rotation
+        angle_deg = np.rad2deg(angle)
+
+        if (0 <= angle_deg < 90) or (270 <= angle_deg <= 360):
+            rotation = -angle_deg
+        else:
+            rotation = 180 - angle_deg
+
+        ax.text(
+            angle,
+            max_radius + ring_text_spacing,
+            wedge_labels[idx],
+            rotation=rotation,
+            rotation_mode="anchor",
+            transform=ax.transData,
+            fontdict={"fontsize": 11 * font_scale_factor},
+            ha="center",
+            va="center",
+        )
+
+
 def add_text(
     ax: Axes, x: float, y: float, text: Optional[str] = None, **kwargs
 ) -> Text:
@@ -110,7 +170,73 @@ def add_text(
     return ax.text(x, y, s, **kwargs)
 
 
-def assign_ring_wedge_columns(
+def aggregate_temporal_columns(
+        data: DataFrame,
+        agg_column: str,
+        agg: Aggregation,
+        mode: Mode
+) -> DataFrame:
+    """Aggregate values in agg_column using pass aggregate function.
+
+    Groups the DataFrame by the temporal 'ring' and 'wedge' columns,
+    before applying the aggregate function to the chosen aggregation
+    column.
+
+    NOTE: The 'ring' & 'wedge' columns are assigned by the utility function
+    assign_temporal_columns.
+
+    Args:
+        data (DataFrame): DataFrame containing data to aggregate.
+        agg_column (str): DataFrame Column to aggregate.
+        agg (Aggregation): Aggregation function; 'count', 'mean', 'median',
+            'mode' & 'sum'.
+        mode (Mode): A mode key representing the temporal bins used in the
+            chart; 'YEAR_MONTH', 'YEAR_WEEK', 'WEEK_DAY', 'DOW_HOUR' &
+            'DAY_HOUR'.
+
+    Raises:
+        ModeError: Unexpected mode value is passed.
+        ValueError: Missing 'ring' & 'wedge' columns.
+
+    Returns:
+        A DataFrame with aggregate values in a new column named after the
+        aggregate function.
+    """
+    columns = ["ring", "wedge"]
+    if not set(columns).issubset(data.columns):
+        raise ValueError(f"Expected DataFrame columns: {columns}")
+    
+    match mode:
+        case "YEAR_MONTH":
+            unique_wedges = tuple(range(1, 13))
+        case "YEAR_WEEK":
+            unique_wedges = range(1, 53)
+        case "WEEK_DAY":
+            unique_wedges = range(0, 7)
+        case "DOW_HOUR":
+            unique_wedges = range(0, 24)
+        case "DAY_HOUR":
+            unique_wedges = range(0, 24)
+        case _:
+            raise ModeError(mode, get_args(Mode))
+    
+    # groupby 'ring' & 'wedge' values and apply aggregate function agg
+    data_agg = data.groupby(columns, as_index=False)[agg_column].agg(agg)
+    data_agg = data_agg.set_axis([*columns, agg], axis="columns")
+
+    # index with all possible combinations of ring & wedge values
+    product_idx = MultiIndex.from_product(
+        [data_agg["ring"].unique(), unique_wedges], names=columns
+    )
+
+    # populate any rows for missing ring/wedge combinations
+    data_agg = data_agg.set_index(columns).reindex(product_idx).reset_index()
+
+    # replace NaN values created for missing missing ring/wedge combinations
+    return data_agg.fillna(0)
+
+
+def assign_temporal_columns(
     data: DataFrame, date_column: str, mode: Mode
 ) -> DataFrame:
     """Assign ring & wedge columns to a DataFrame based on mode.
@@ -135,7 +261,8 @@ def assign_ring_wedge_columns(
     # year | January - December
     if mode == "YEAR_MONTH":
         mode_map[mode]["ring"] = data[date_column].dt.year
-        mode_map[mode]["wedge"] = data[date_column].dt.month_name()
+        mode_map[mode]["wedge"] = data[date_column].dt.month
+        #data[date_column].dt.month_name()
     # year | weeks 1 - 52
     if mode == "YEAR_WEEK":
         mode_map[mode]["ring"] = data[date_column].dt.year
@@ -147,7 +274,7 @@ def assign_ring_wedge_columns(
         week = data[date_column].dt.isocalendar().week
         year = data[date_column].dt.year
         mode_map[mode]["ring"] = week + year * 100
-        mode_map[mode]["wedge"] = data[date_column].dt.day_name()
+        mode_map[mode]["wedge"] = data[date_column].dt.day_of_week
     # days 1 - 7 (Monday - Sunday) | 00:00 - 23:00
     if mode == "DOW_HOUR":
         mode_map[mode]["ring"] = data[date_column].dt.day_of_week
@@ -170,7 +297,6 @@ def get_figure_dimensions(wedges: int) -> tuple[float, float]:
     NOTE: The minimum figure size is capped at (10.0, 10.0).
 
     Example:
-      # 'DOW_HOUR' mode has 24 wedges for each of the 7 rings
       >>> calculate_figure_dimensions(168)
       (11, 11)
 
